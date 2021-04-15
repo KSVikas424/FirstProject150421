8c96fb73f34fc198defcf288eecb4eba82de04cc
ksvikas424 <srinivasvikask@gmail.com>
2021-04-15 11:02:55 +0530

    Object Class

---
+++ src/main/kotlin/cao.kt
@@ -0,0 +1,12 @@
+class Company {
+    var news: String = "company is launching a new product"
+    companion object Media {
+        fun broadcastNews(){ //static method
+            println("company news")
+        }
+    }
+}
+
+fun main() {
+    Company.broadcastNews()
+}
